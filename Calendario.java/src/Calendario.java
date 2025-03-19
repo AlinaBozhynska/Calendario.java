810e8a546ab397c7567ca384b234645c6316bf77
iambarta <lorenzobartaletti.design@gmail.com>
2025-03-19 11:44:20 +0100

    Aggiunta weekend

--- Calendario.java/src/Calendario.java
+++ Calendario.java/src/Calendario.java
@@ -3,6 +3,9 @@ public class Calendario {
     public int giorno;
     public String mese;
 
+    // feb: 15-16, 22-23; mar: 1-2, 8-9, 15-16, 22-23, 29-30; apr: 5-6, 12-13,
+    // 19-20, 26-27; mag: 3-4, 10-11, 17,18.
+
     public boolean verifyDate() {
         switch (mese) {
             case "gennaio", "marzo", "maggio", "luglio", "agosto", "ottobre", "dicembre":
@@ -32,61 +35,72 @@ public class Calendario {
     }
 
     public void printModule() {
-        if (verifyDate()) {
-            switch (mese) {
-                case "gennaio":
+        if (verifyDate() == false) {
+            return;
+        }
+        switch (mese) {
+            case "gennaio":
+                System.out.println("Corso non ancora avviato");
+                break;
+            case "febbraio":
+                if (giorno == 15 || giorno == 16 || giorno == 22 || giorno == 23) {
+                    System.out.println("Il weekend non si lavora!!");
+                } else if (giorno >= 1 && giorno <= 12) {
                     System.out.println("Corso non ancora avviato");
-                    break;
-                case "febbraio":
-                    if (giorno >= 1 && giorno <= 12) {
-                        System.out.println("Corso non ancora avviato");
-                    } else if (giorno >= 13) {
-                        System.out.println("Web");
-                    }
-                    break;
-                case "marzo":
-                    if (giorno <= 4) {
-                        System.out.println("Web");
-                    } else if (giorno == 5) {
-                        System.out.println("DASA");
-                    } else if ((giorno >= 6 && giorno <= 10) || (giorno >= 12)) {
-                        System.out.println("JAVA");
-                    } else if (giorno == 11) {
-                        System.out.println("GitHub");
-                    }
-                    break;
-                case "aprile":
-                    if (giorno == 25) {
-                        System.out.println("Anniversario dell'indipendenza!!");
-                    } else if (giorno == 21) {
-                        System.out.println("Pasqua!!");
-                    } else if(giorno <= 8) {
-                        System.out.println("Java"); 
-                    } else if (giorno >= 9 && giorno <= 17) {
-                        System.out.println("Database");
-                    } else if (giorno >= 18 && giorno <= 23) {
-                        System.out.println("JDBC");
-                    } else if (giorno >= 24) {
-                        System.out.println("Spring");
-                    }
-                    break;
-                case "maggio":
-                    if (giorno == 1) {
-                        System.out.println("Festa dei lavoratori!!");
-                    } else if (giorno <= 8) {
-                        System.out.println("Spring");
-                    } else if (giorno >= 9 && giorno <= 21) {
-                        System.out.println("Project work");
-                    } else if (giorno >= 22 && giorno <= 30) {
-                        System.out.println("Corso terminato!!!");
-                    } else if (giorno == 31) {
-                        System.out.println("Corso non presente");
-                    }
-                    break;
-                default:
+                } else if (giorno >= 13) {
+                    System.out.println("Web");
+                }
+                break;
+            case "marzo":
+                if ((giorno == 1 || giorno == 2 || giorno == 8 || giorno == 9 || giorno == 15 || giorno == 16
+                        || giorno == 22 || giorno == 23 || giorno == 29 || giorno == 30)) {
+                    System.out.println("Il weekend non si lavora!!");
+                } else if (giorno <= 4) {
+                    System.out.println("Web");
+                } else if (giorno == 5) {
+                    System.out.println("DASA");
+                } else if ((giorno >= 6 && giorno <= 10) || (giorno >= 12)) {
+                    System.out.println("JAVA");
+                } else if (giorno == 11) {
+                    System.out.println("GitHub");
+                }
+                break;
+            case "aprile":
+                if (giorno == 5 || giorno == 6 || giorno == 12 || giorno == 13 || giorno == 19 || giorno == 20
+                        || giorno == 26 || giorno == 27) {
+                    System.out.println("Il weekend non si lavora!!");
+                } else if (giorno == 25) {
+                    System.out.println("Anniversario dell'indipendenza!!");
+                } else if (giorno == 21) {
+                    System.out.println("Pasqua!!");
+                } else if (giorno <= 8) {
+                    System.out.println("Java");
+                } else if (giorno >= 9 && giorno <= 17) {
+                    System.out.println("Database");
+                } else if (giorno >= 18 && giorno <= 23) {
+                    System.out.println("JDBC");
+                } else if (giorno >= 24) {
+                    System.out.println("Spring");
+                }
+                break;
+            case "maggio":
+                if (giorno == 3 || giorno == 4 || giorno == 10 || giorno == 11 || giorno == 17 || giorno == 18) {
+                    System.out.println("Il weekend non si lavora!!");
+                } else if (giorno == 1) {
+                    System.out.println("Festa dei lavoratori!!");
+                } else if (giorno <= 8) {
+                    System.out.println("Spring");
+                } else if (giorno >= 9 && giorno <= 21) {
+                    System.out.println("Project work");
+                } else if (giorno >= 22 && giorno <= 30) {
+                    System.out.println("Corso terminato!!!");
+                } else if (giorno == 31) {
                     System.out.println("Corso non presente");
-                    break;
-            }
+                }
+                break;
+            default:
+                System.out.println("Corso non presente");
+                break;
         }
 
     }
--- Calendario.java/src/CalendarioTest.java
+++ Calendario.java/src/CalendarioTest.java
@@ -2,8 +2,8 @@ public class CalendarioTest {
 
     public static void main(String[] args) {
         Calendario calendario1 = new Calendario();
-        calendario1.mese = "aprile";
-        calendario1.giorno = 25;
+        calendario1.mese = "maggio";
+        calendario1.giorno = 3;
 
         calendario1.printModule();
 
