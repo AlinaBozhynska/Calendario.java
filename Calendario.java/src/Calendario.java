aec0668b3612b8988855506859df3b459a9a018f
AlinaBozhynska <bozhynska.alina@gmail.com>
2025-03-19 10:16:56 +0100

    Verify Date

--- Calendario.java/src/Calendario.java
+++ Calendario.java/src/Calendario.java
@@ -1,3 +1,34 @@
 public class Calendario {
+
+    public int giorno;
+    public String mese;
+
+    public boolean verifyDate() {
+        switch(mese) {
+            case "gennaio", "marzo", "maggio", "luglio", "agosto", "ottobre", "dicembre":
+            if(giorno >= 1 && giorno <= 31) { 
+                return true;
+            } 
+            System.out.println("Giorno immesso non valido!");           
+            break;
+
+            case "febbraio": 
+            if(giorno >= 1 && giorno <= 28) {
+                return true;
+            }
+            System.out.println("Giorno immesso non valido!");
+            break;
+
+            case "aprile", "giugno", "settembre", "novembre":
+            if(giorno >= 1 && giorno <= 30) {
+                return true;
+            }
+            System.out.println("Giorno immesso non valido!");
+            break;
+            default: 
+            System.out.println("Mese immesso non valido!");
+        } 
+        return false;
+    } 
     
 }
--- Calendario.java/src/CalendarioTest.java
+++ Calendario.java/src/CalendarioTest.java
@@ -1,3 +1,12 @@
 public class CalendarioTest {
+
+    public static void main(String[] args) {
+        Calendario calendario1 = new Calendario();
+        calendario1.mese = "febbraio";
+        calendario1.giorno = 30;
+
+        System.out.println(calendario1.verifyDate());
+
+    }
     
 }
